5d002497c3878b5d353a805cb56d5f07a2964006
Electro Mysterio <raydiati0n@hotmail.com>
2019-03-15 22:52:35 +0600

    media folder deleted

--- electromysterio/settings.py
+++ electromysterio/settings.py
@@ -145,9 +145,8 @@ EMAIL_USE_SSL = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'static'),  
 )
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 
 django_heroku.settings(locals())
